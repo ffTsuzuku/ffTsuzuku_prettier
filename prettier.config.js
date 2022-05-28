1db09dfe70ea32f76397cb70aeabbadad360b300
ffTsuzuku <gagandeepsandhu@protonmail.com>
2022-05-28 04:37:22 -0400

    init commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSameLine: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxSingleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'none',
+}
